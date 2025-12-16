8c7171b81a4f4d4a745047431cebca3db55e2553
JangMuho <muhojang.dev@gmail.com>
2025-12-16 14:37:15 +0900

    resources: 바텀 네비게이션 아이콘 및 문자열 리소스 추가

--- app/src/main/java/com/boostcamp/and03/ui/core/navigation/MainBottomTab.kt
+++ app/src/main/java/com/boostcamp/and03/ui/core/navigation/MainBottomTab.kt
@@ -1,34 +1,31 @@
 package com.boostcamp.and03.ui.core.navigation
 
+import com.boostcamp.and03.R
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 enum class MainBottomTab(
-    @param:DrawableRes val selectedIconId: Int?,
-    @param:DrawableRes val unselectedIconId: Int?,
-    @param:StringRes val iconTextId: Int?,
-    @param:StringRes val titleTextId: Int?,
+    @param:DrawableRes val selectedIconId: Int,
+    @param:DrawableRes val unselectedIconId: Int,
+    @param:StringRes val titleTextId: Int,
     val route: MainTabRoute
 ) {
     BOOKLIST(
-        null,
-        null,
-        null,
-        null,
+        R.drawable.ic_collections_bookmark_filled,
+        R.drawable.ic_collections_bookmark_outlined,
+        R.string.bottom_tab_booklist,
         MainTabRoute.Booklist
     ),
-    ADDBOOK(
-        null,
-        null,
-        null,
-        null,
+    ADD_BOOK(
+        R.drawable.ic_library_add_filled,
+        R.drawable.ic_library_add_outlined,
+        R.string.bottom_tab_add_book,
         MainTabRoute.AddBook
     ),
-    MYPAGE(
-        null,
-        null,
-        null,
-        null,
+    MY_PAGE(
+        R.drawable.ic_account_filled,
+        R.drawable.ic_account_outlined,
+        R.string.bottom_tab_my_page,
         MainTabRoute.MyPage
     )
 }
