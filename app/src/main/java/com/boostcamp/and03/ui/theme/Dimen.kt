14f8954ea156d4cecaed45b8d0f102c3cea7371b
JangMuho <muhojang.dev@gmail.com>
2025-12-16 18:21:59 +0900

    feat: 공용으로 사용할 Dimension 값 정의

---
+++ app/src/main/java/com/boostcamp/and03/ui/theme/Dimen.kt
@@ -0,0 +1,23 @@
+package com.boostcamp.and03.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimensions {
+    val PADDING_XS = 4.dp
+    val PADDING_S = 8.dp
+    val PADDING_M = 12.dp
+    val PADDING_L = 16.dp
+    val PADDING_XL = 20.dp
+    val PADDING_2XL = 24.dp
+    val PADDING_3XL = 28.dp
+    val PADDING_4XL = 32.dp
+
+    val PADDING_5XL = 36.dp
+    val PADDING_6XL = 40.dp
+    val PADDING_7XL = 44.dp
+    val PADDING_8XL = 48.dp
+    val PADDING_9XL = 52.dp
+    val PADDING_10XL = 56.dp
+    val PADDING_11XL = 60.dp
+    val PADDING_12XL = 64.dp
+}
