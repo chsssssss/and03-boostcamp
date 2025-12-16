9819187ae58f08cfe6fdae5e97df24927d742cf8
JangMuho <muhojang.dev@gmail.com>
2025-12-16 18:25:14 +0900

    refactor: BookUIModel의 authors 타입을 ImmutableList로 변경

--- app/src/main/java/com/boostcamp/and03/ui/screen/booklist/model/BookUIModel.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/booklist/model/BookUIModel.kt
@@ -1,12 +1,14 @@
 package com.boostcamp.and03.ui.screen.booklist.model
 
 import com.boostcamp.and03.data.feature.booklist.model.KakaoDocumentDTO
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BookUIModel(
     val title: String,
-    val authors: List<String>,
+    val authors: ImmutableList<String>,
     val publisher: String,
     val thumbnail: String
 ) {
@@ -16,7 +18,7 @@ data class BookUIModel(
 
 fun KakaoDocumentDTO.toUiModel() = BookUIModel(
     title = title,
-    authors = authors,
+    authors = authors.toImmutableList(),
     publisher = publisher,
     thumbnail = thumbnail
 )
