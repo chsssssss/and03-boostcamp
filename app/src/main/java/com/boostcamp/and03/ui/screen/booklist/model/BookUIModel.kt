204b7cfc191afb77c29b39d71dace48715e5ccf6
JangMuho <muhojang.dev@gmail.com>
2025-12-16 17:04:05 +0900

    feat: BookUIModel 및 KakaoDocumentDTO로부터의 매퍼 추가

---
+++ app/src/main/java/com/boostcamp/and03/ui/screen/booklist/model/BookUIModel.kt
@@ -0,0 +1,19 @@
+package com.boostcamp.and03.ui.screen.booklist.model
+
+import com.boostcamp.and03.data.feature.booklist.model.KakaoDocumentDTO
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BookUIModel(
+    val title: String,
+    val authors: List<String>,
+    val publisher: String,
+    val thumbnail: String?
+)
+
+fun KakaoDocumentDTO.toUiModel() = BookUIModel(
+    title = title,
+    authors = authors,
+    publisher = publisher,
+    thumbnail = thumbnail
+)
