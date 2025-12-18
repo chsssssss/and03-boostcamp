a4bc748ecb282e63572e938dee72816193def90d
chsssssss <chaheesun42@gmail.com>
2025-12-19 05:30:41 +0900

    remove: 프로토타입용 더미 데이터 삭제

--- app/src/main/java/com/boostcamp/and03/ui/screen/prototype/navigation/PrototypeNavigation.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/prototype/navigation/PrototypeNavigation.kt
@@ -19,17 +19,6 @@ import kotlinx.serialization.Serializable
 fun PrototypeNavHost() {
     val navController = rememberNavController()
 
-    var items by remember {
-        mutableStateOf(MemoGraph().
-            addMemo(MemoNode("1", "인물 A", "설명 A", Offset(100f, 200f)))
-            .addMemo(MemoNode("2", "인물 B", "설명 B", Offset(700f, 200f)))
-            .addMemo(MemoNode("3", "인물 C", "설명 C", Offset(100f, 600f)))
-            .addMemo(MemoNode("4", "인물 D", "설명 D", Offset(700f, 600f)))
-            .addMemo(MemoNode("5", "인물 E", "설명 E", Offset(100f, 1000f)))
-            .addMemo(MemoNode("6", "인물 F", "설명 F", Offset(700f, 1000f)))
-        )
-    }
-
     NavHost(
         navController = navController,
         startDestination = PrototypeRoute.Canvas
