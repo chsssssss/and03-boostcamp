b9d58ae0b40881b319cd8eec42ca52b013a882b8
chsssssss <chaheesun42@gmail.com>
2025-12-28 18:40:50 +0900

    resources: Material3 타이포그래피 시스템 정의
    - 앱 전반에 사용할 Display, Headline, Title, Body, Label 스타일 구성 추가 및 수정

--- app/src/main/java/com/boostcamp/and03/ui/theme/Type.kt
+++ app/src/main/java/com/boostcamp/and03/ui/theme/Type.kt
@@ -8,27 +8,89 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+
+    /* ================= Display ================= */
+
+    displayLarge = TextStyle(
+        fontSize = 48.sp,
+        fontWeight = FontWeight.Bold
     ),
+
+    displayMedium = TextStyle(
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
+    ),
+
+    displaySmall = TextStyle(
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold
+    ),
+
+    /* ================= Headline ================= */
+
+    headlineLarge = TextStyle(
+        fontSize = 28.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+
+    headlineMedium = TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+
+    headlineSmall = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+
+    /* ================= Title ================= */
+
+    titleLarge = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Medium
+    ),
+
+    titleMedium = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium
+    ),
+
+    titleSmall = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium
+    ),
+
+    /* ================= Body ================= */
+
+    bodyLarge = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal
+    ),
+
+    bodyMedium = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal
+    ),
+
+    bodySmall = TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Normal
+    ),
+
+    /* ================= Label ================= */
+
+    labelLarge = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium
+    ),
+
+    labelMedium = TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Medium
+    ),
+
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontWeight = FontWeight.Normal
     )
-    */
 )
