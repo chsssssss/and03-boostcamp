f8ed030b0a7c1ecce9772f93ffc72b069f4f002b
JangMuho <muhojang.dev@gmail.com>
2025-12-29 20:38:40 +0900

    feat: 공용 API 응답 모델 정의

---
+++ app/src/main/java/com/boostcamp/and03/data/model/response/ApiResponse.kt
@@ -0,0 +1,11 @@
+package com.boostcamp.and03.data.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiResponse<T>(
+    val success: Boolean,
+    val data: T? = null,
+    val message: String? = null,
+    val errorCode: String? = null
+)
