456ad8c59659395dd31ec641be3b32744b309b64
JangMuho <muhojang.dev@gmail.com>
2025-12-29 20:16:56 +0900

    feat: 네이버 도서 검색 API 네트워크 모듈 추가 - Naver Book Search API 연동을 위한 Retrofit, OkHttpClient 등 Hilt 모듈을 구성합니다. - 기존 Kakao API 모델 종속성 제거, Naver API 응답에 맞는 UI 모델로 수정 예정

---
+++ app/src/main/java/com/boostcamp/and03/data/di/NaverBookSearchNetworkModule.kt
@@ -0,0 +1,72 @@
+package com.boostcamp.and03.data.di
+
+import com.boostcamp.and03.BuildConfig
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Qualifier
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class NaverBookSearchRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class NaverBookSearchOkHttpClient
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NaverBookSearchNetworkModule {
+    private const val BASE_URL = "https://openapi.naver.com"
+    private const val CLIENT_ID = BuildConfig.NAVER_CLIENT_ID
+    private const val CLIENT_SECRET = BuildConfig.NAVER_CLIENT_SECRET
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    private val contentType = "application/json".toMediaType()
+
+    @NaverBookSearchOkHttpClient
+    @Provides
+    @Singleton
+    fun provideNaverBookSearchOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("X-Naver-Client-Id", CLIENT_ID)
+                    .addHeader("X-Naver-Client-Secret", CLIENT_SECRET)
+                    .build()
+                chain.proceed(newRequest)
+            }.build()
+
+    @NaverBookSearchRetrofit
+    @Provides
+    @Singleton
+    fun provideNaverBookSearchRetrofit(
+        json: Json,
+        @NaverBookSearchOkHttpClient okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideNaverBookSearchService(
+        @NaverBookSearchRetrofit retrofit: Retrofit
+    ): NaverBookSearchService =
+        retrofit.create(NaverBookSearchService::class.java)
+}
