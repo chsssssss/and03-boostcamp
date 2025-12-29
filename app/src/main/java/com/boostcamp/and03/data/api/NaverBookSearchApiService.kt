42219211fab81adfbaee25a4cc3d93f33128a15d
JangMuho <muhojang.dev@gmail.com>
2025-12-29 21:13:25 +0900

    refactor: Naver 책 검색 API 관련 상수 외부 파일로 분리

--- app/src/main/java/com/boostcamp/and03/data/api/NaverBookSearchApiService.kt
+++ app/src/main/java/com/boostcamp/and03/data/api/NaverBookSearchApiService.kt
@@ -4,21 +4,12 @@ import com.boostcamp.and03.data.model.response.NaverBookSearchResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private object NaverBookSearchApiServiceValue {
-    const val PAGE_DEFAULT = 1
-    const val PAGE_MAX = 1000
-    const val SIZE_DEFAULT = 10
-    const val SIZE_MAX = 100
-    const val SORT_SIMILARITY = "sim"
-    const val SORT_DATE = "date"
-}
-
 interface NaverBookSearchApiService {
     @GET("v1/search/book.json")
     suspend fun loadBooks(
         @Query("query") query: String,
-        @Query("display") display: Int = NaverBookSearchApiServiceValue.SIZE_DEFAULT,
-        @Query("start") start: Int = NaverBookSearchApiServiceValue.PAGE_DEFAULT,
-        @Query("sort") sort: String = NaverBookSearchApiServiceValue.SORT_SIMILARITY
+        @Query("display") display: Int = NaverBookSearchApiConstants.SIZE_DEFAULT,
+        @Query("start") start: Int = NaverBookSearchApiConstants.START_DEFAULT,
+        @Query("sort") sort: String = NaverBookSearchApiConstants.SORT_SIMILARITY
     ): NaverBookSearchResponse
 }
