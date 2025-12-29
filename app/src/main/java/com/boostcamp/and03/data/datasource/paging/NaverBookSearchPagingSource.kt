35e879c0079f91ff1c1bfedeb434a2d2a2e93d23
JangMuho <muhojang.dev@gmail.com>
2025-12-29 21:05:59 +0900

    feat: 네이버 책 검색 API 페이징 소스 구현

---
+++ app/src/main/java/com/boostcamp/and03/data/datasource/paging/NaverBookSearchPagingSource.kt
@@ -0,0 +1,44 @@
+package com.boostcamp.and03.data.datasource.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.boostcamp.and03.data.datasource.remote.NaverBookSearchRemoteDataSource
+import com.boostcamp.and03.data.model.response.NaverBookItem
+
+class NaverBookSearchPagingSource(
+    private val remoteDataSource: NaverBookSearchRemoteDataSource,
+    private val query: String
+): PagingSource<Int, NaverBookItem>() {
+
+    override suspend fun load(
+        params: LoadParams<Int>
+    ): LoadResult<Int, NaverBookItem> {
+
+        val display = params.loadSize
+        val start = params.key ?: 1
+
+        val result = remoteDataSource.loadBooks(
+            query = query,
+            display = display,
+            start = start,
+        )
+
+        return LoadResult.Page(
+            data = result.items,
+            prevKey = if (start == 1) null else maxOf(1, start - display),
+            nextKey = if (result.hasNext) result.nextStart else null
+        )
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, NaverBookItem>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val closestPage = state.closestPageToPosition(anchorPosition)
+            closestPage?.prevKey?.plus(state.config.pageSize)
+                ?: closestPage?.nextKey?.minus(state.config.pageSize)
+        }
+    }
+
+    companion object {
+        private const val NAVER_STARTING_ITEM_INDEX = 1
+    }
+}
