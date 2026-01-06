993937e7e2f1f4869d39b1ae869247d18d414042
JangMuho <muhojang.dev@gmail.com>
2026-01-06 17:25:59 +0900

    fix: MaterialTheme 대신 앱 커스텀 테마 주입 - MaterialTheme에 커스텀 테마 스키마 대신 MaterialTheme.colorScheme가 주입되고 있었습니다.

--- app/src/main/java/com/boostcamp/and03/ui/theme/Theme.kt
+++ app/src/main/java/com/boostcamp/and03/ui/theme/Theme.kt
@@ -2,27 +2,46 @@ package com.boostcamp.and03.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-
-private val LightAnd03Scheme = lightAnd03Colors
-private val DarkAnd03Scheme = darkMainColors
 
 @Composable
 fun And03Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val mainColorScheme = if(darkTheme) DarkAnd03Scheme else LightAnd03Scheme
+    val mainColors = if(darkTheme) darkMainColors else lightMainColors
+
+    val mainColorScheme = lightColorScheme(
+        primary = mainColors.primary,
+        onPrimary = mainColors.onPrimary,
+        primaryContainer = mainColors.primaryContainer,
+        onPrimaryContainer = mainColors.onPrimaryContainer,
+        secondary = mainColors.secondary,
+        onSecondary = mainColors.onSecondary,
+        secondaryContainer = mainColors.secondaryContainer,
+        onSecondaryContainer = mainColors.onSecondaryContainer,
+        background = mainColors.background,
+        onBackground = mainColors.onBackground,
+        surface = mainColors.surface,
+        onSurface = mainColors.onSurface,
+        surfaceVariant = mainColors.surfaceVariant,
+        onSurfaceVariant = mainColors.onSurfaceVariant,
+        error = mainColors.error,
+        onError = mainColors.onError,
+        outline = mainColors.outline,
+        outlineVariant = mainColors.outlineVariant,
+        scrim = mainColors.scrim
+    )
 
     CompositionLocalProvider(
-        LocalAnd03Colors provides mainColorScheme,
+        LocalAnd03Colors provides mainColors,
         LocalAnd03Typography provides and03Typography,
         LocalAnd03Shapes provides and03Shapes
     ) {
         MaterialTheme(
-            colorScheme = colorScheme,
+            colorScheme = mainColorScheme,
             typography = Typography,
             content = content
         )
