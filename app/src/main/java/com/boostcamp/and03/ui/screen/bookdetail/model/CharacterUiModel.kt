ee5e965fc6d27f1fbd2bad36c263f03f1d7f2e5a
chsssssss <chaheesun42@gmail.com>
2026-01-07 02:34:04 +0900

    feat: 도서 상세 화면 탭 및 등장인물 UI 모델 추가
    - 도서 상세 정보를 분류하는 BookDetailTab enum 및 등장인물 정보를 담는 CharacterUiModel 정의

---
+++ app/src/main/java/com/boostcamp/and03/ui/screen/bookdetail/model/CharacterUiModel.kt
@@ -0,0 +1,11 @@
+package com.boostcamp.and03.ui.screen.bookdetail.model
+
+import androidx.compose.ui.graphics.Color
+
+
+data class CharacterUiModel(
+    val name: String,
+    val role: String,
+    val iconColor: Color,
+    val description: String
+)
