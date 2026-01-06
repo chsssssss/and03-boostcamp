1d2e86e2b27340525fb01429eabfa45bdd80494e
JangMuho <muhojang.dev@gmail.com>
2026-01-06 16:40:44 +0900

    feat: 책 검색 화면에 임시로 사용할 TopAppBar 구현

---
+++ app/src/main/java/com/boostcamp/and03/ui/component/SearchTopBar.kt
@@ -0,0 +1,61 @@
+package com.boostcamp.and03.ui.component
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.boostcamp.and03.R
+import com.boostcamp.and03.ui.theme.And03Theme
+
+// 임시로 사용할 탑 바
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchTopBar(
+    title: String,
+    onBackClick: () -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(
+                onClick = onBackClick
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_keyboard_arrow_left_filled),
+                    contentDescription = stringResource(R.string.content_description_go_back)
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = onSaveClick
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_check_filled),
+                    contentDescription = stringResource(R.string.content_description_save_button)
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun SearchTopBarPreview() {
+    And03Theme {
+        SearchTopBar(
+            title = "책 검색",
+            onBackClick = {},
+            onSaveClick = {}
+        )
+    }
+}
