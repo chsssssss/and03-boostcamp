fcf9fa99b73525832291bccb682479cd30410eeb
JangMuho <muhojang.dev@gmail.com>
2026-01-08 00:25:38 +0900

    rename: loadBookInfo 함수명을 loadBookPage로 변경

--- app/src/main/java/com/boostcamp/and03/data/datasource/remote/AladinBookLookUpRemoteDataSource.kt
+++ app/src/main/java/com/boostcamp/and03/data/datasource/remote/AladinBookLookUpRemoteDataSource.kt
@@ -3,7 +3,7 @@ package com.boostcamp.and03.data.datasource.remote
 import com.boostcamp.and03.data.model.response.AladinBookLookUpResponse
 
 interface AladinBookLookUpRemoteDataSource {
-    suspend fun loadBookInfo(
+    suspend fun loadBookPage(
         itemId: String
     ): AladinBookLookUpResponse
 }
