d344e6aea850d03ed1061898d7cb94a0359b172e
JangMuho <muhojang.dev@gmail.com>
2026-01-07 23:04:30 +0900

    feat: 알라딘 도서 조회 API 관련 네트워크 모듈 추가

---
+++ app/src/main/java/com/boostcamp/and03/data/di/AladinBookLookUpModule.kt
@@ -0,0 +1,19 @@
+package com.boostcamp.and03.data.di
+
+import com.boostcamp.and03.data.datasource.remote.NaverBookSearchRemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AladinBookLookUpModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindNaverBookSearchRemoteDataSource(
+        impl: AladinBookLookUpRemoteDataSourceImpl
+    ): AladinBookLookUpRemoteDataSource
+}
