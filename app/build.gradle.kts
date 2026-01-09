b2e713a64b0187b73c4cb59b3ad608acef70fdab
JangMuho <muhojang.dev@gmail.com>
2026-01-09 11:59:13 +0900

    build: 앱 버전 정보 업데이트 - versionCode 및 versionName 상향 조정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.boostcamp.and03"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
