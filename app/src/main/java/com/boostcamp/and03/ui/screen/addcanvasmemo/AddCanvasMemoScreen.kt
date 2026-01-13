68b60d5306e0a33d851c1ed09ca601a6b06d1104
JangMuho <muhojang.dev@gmail.com>
2026-01-13 15:20:54 +0900

    feat: 텍스트 및 캔버스 메모 추가 화면 네비게이션 구현 - Route에 AddTextMemo, AddCanvasMemo 정의 - 각 화면에 Route 컴포저블 도입 및 ViewModel 연결 - NavGraph 설정을 위한 확장 함수 추가

--- app/src/main/java/com/boostcamp/and03/ui/screen/addcanvasmemo/AddCanvasMemoScreen.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/addcanvasmemo/AddCanvasMemoScreen.kt
@@ -1,10 +1,21 @@
 package com.boostcamp.and03.ui.screen.addcanvasmemo
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 
 @Composable
-fun AddCanvasMemoScreen(
+fun AddCanvasMemoRoute(
+    navigateToBack: () -> Unit,
+    viewModel: AddCanvasMemoViewModel = hiltViewModel()
+) {
+    AddCanvasMemoScreen(
+        navigateToBack = navigateToBack
+    )
+}
 
+@Composable
+private fun AddCanvasMemoScreen(
+    navigateToBack: () -> Unit,
 ) {
 
 }
--- app/src/main/java/com/boostcamp/and03/ui/screen/addtextmemo/AddTextMemoScreen.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/addtextmemo/AddTextMemoScreen.kt
@@ -1,10 +1,21 @@
 package com.boostcamp.and03.ui.screen.addtextmemo
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 
 @Composable
-fun AddTextMemoScreen(
+fun AddTextMemoRoute(
+    navigateToBack: () -> Unit,
+    viewModel: AddTextMemoViewModel = hiltViewModel()
+) {
+    AddTextMemoScreen(
+        navigateToBack = navigateToBack
+    )
+}
 
+@Composable
+private fun AddTextMemoScreen(
+    navigateToBack: () -> Unit
 ) {
 
 }
