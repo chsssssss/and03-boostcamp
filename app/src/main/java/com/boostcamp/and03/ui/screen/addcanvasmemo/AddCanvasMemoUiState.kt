faecc744db40f7ba716845b4afa05b80a2eee54a
JangMuho <muhojang.dev@gmail.com>
2026-01-13 16:56:28 +0900

    feat: MemoUiState 프로퍼티 기본값 설정 및 isSaveable 프로퍼티 추가

--- app/src/main/java/com/boostcamp/and03/ui/screen/addcanvasmemo/AddCanvasMemoUiState.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/addcanvasmemo/AddCanvasMemoUiState.kt
@@ -3,5 +3,6 @@ package com.boostcamp.and03.ui.screen.addcanvasmemo
 data class AddCanvasMemoUiState(
     val title: String,
     val startPage: Int,
-    val endPage: Int
+    val endPage: Int,
+    val isSaveable: Boolean = false
 )
--- app/src/main/java/com/boostcamp/and03/ui/screen/addtextmemo/AddTextMemoUiState.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/addtextmemo/AddTextMemoUiState.kt
@@ -1,8 +1,9 @@
 package com.boostcamp.and03.ui.screen.addtextmemo
 
 data class AddTextMemoUiState(
-    val title: String,
-    val content: String,
-    val startPage: Int,
-    val endPage: Int
+    val title: String = "",
+    val content: String = "",
+    val startPage: Int = 0,
+    val endPage: Int = 0,
+    val isSaveable: Boolean = false
 )
