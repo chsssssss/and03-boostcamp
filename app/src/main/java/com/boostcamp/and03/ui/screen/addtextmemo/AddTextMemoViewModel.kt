0d813c147273a72f09534674d5baaeaa29acd246
JangMuho <muhojang.dev@gmail.com>
2026-01-13 16:59:04 +0900

    feat: AddCanvasMemoUiState 기본값 설정, ViewModel 저장 함수 템플릿 추가

--- app/src/main/java/com/boostcamp/and03/ui/screen/addtextmemo/AddTextMemoViewModel.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/addtextmemo/AddTextMemoViewModel.kt
@@ -2,11 +2,18 @@ package com.boostcamp.and03.ui.screen.addtextmemo
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class AddTextMemoViewModel @Inject constructor(
 
 ): ViewModel() {
+    private val _uiState = MutableStateFlow(AddTextMemoUiState())
+    val uiState = _uiState.asStateFlow()
 
+    fun saveTextMemo() {
+        // TODO: 텍스트 메모 저장 기능 구현
+    }
 }
