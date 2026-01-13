4fe08e964979620e7737b232ddd597a291295cf8
JangMuho <muhojang.dev@gmail.com>
2026-01-14 02:57:43 +0900

    feat: 본문 입력 섹션 컴포넌트 구현 - 레이블을 바꿀 수 있음, 텍스트 메모 추가 및 인상깊은 문장 추가 시에도 활용 가능

---
+++ app/src/main/java/com/boostcamp/and03/ui/component/ContentInputSection.kt
@@ -0,0 +1,70 @@
+package com.boostcamp.and03.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.boostcamp.and03.R
+import com.boostcamp.and03.ui.theme.And03ComponentSize
+import com.boostcamp.and03.ui.theme.And03Spacing
+import com.boostcamp.and03.ui.theme.And03Theme
+
+private object ContentInputSectionValues {
+    const val MAX_CHARACTER_COUNT = 500
+}
+
+@Composable
+fun ContentInputSection(
+    label: String,
+    content: String,
+    onContentChange: (String) -> Unit,
+    onAddByImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(And03Spacing.SPACE_S)
+    ) {
+        Row(
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Text(
+                text = label,
+                style = And03Theme.typography.labelLarge
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            AddTextByImageButton(onAddByImageClick = onAddByImageClick)
+        }
+
+        OutlinedTextField(
+            value = content,
+            onValueChange = onContentChange,
+            modifier = modifier
+                .fillMaxWidth()
+                .height(And03ComponentSize.TEXT_FIELD_HEIGHT_L),
+            placeholder = { Text(stringResource(id = R.string.add_memo_text_enter_content_placeholder)) },
+            shape = And03Theme.shapes.defaultCorner
+        )
+
+        Text(
+            text = stringResource(
+                id = R.string.add_memo_character_count,
+                content.length,
+                ContentInputSectionValues.MAX_CHARACTER_COUNT
+            ),
+            modifier = Modifier.align(Alignment.End),
+            style = And03Theme.typography.bodySmall,
+            color = And03Theme.colors.onSurfaceVariant
+        )
+    }
+}
