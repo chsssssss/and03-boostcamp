5de748592bd83762d6937507fbf588656b28d795
JangMuho <muhojang.dev@gmail.com>
2026-01-13 16:30:18 +0900

    feat: 메모 작성 화면의 uiState 필드 추가

--- app/src/main/java/com/boostcamp/and03/ui/screen/addcanvasmemo/AddCanvasMemoUiState.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/addcanvasmemo/AddCanvasMemoUiState.kt
@@ -1,5 +1,7 @@
 package com.boostcamp.and03.ui.screen.addcanvasmemo
 
 data class AddCanvasMemoUiState(
-
+    val title: String,
+    val startPage: Int,
+    val endPage: Int
 )
--- app/src/main/java/com/boostcamp/and03/ui/screen/addtextmemo/AddTextMemoUiState.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/addtextmemo/AddTextMemoUiState.kt
@@ -1,5 +1,8 @@
 package com.boostcamp.and03.ui.screen.addtextmemo
 
 data class AddTextMemoUiState(
-
+    val title: String,
+    val content: String,
+    val startPage: Int,
+    val endPage: Int
 )
