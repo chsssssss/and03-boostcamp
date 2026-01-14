0a4ae68b21bdfc12a0958b6c547bca91a0067f23
chanhue <chanhue467@gmail.com>
2026-01-15 02:16:38 +0900

    feat: 구절 추가 ui 구현

--- app/src/main/java/com/boostcamp/and03/MainActivity.kt
+++ app/src/main/java/com/boostcamp/and03/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.boostcamp.and03.ui.component.AddQuoteBottomSheet
 import com.boostcamp.and03.ui.navigation.rememberMainNavigator
+import com.boostcamp.and03.ui.screen.bookdetail.model.QuoteUiModel
 import com.boostcamp.and03.ui.theme.And03Theme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,10 +17,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             And03Theme {
-                val navigator = rememberMainNavigator()
-                MainApp(navigator)
-//                PrototypeNavHost()
+                AddQuoteBottomSheet(quotes = dummyQuotes)
             }
         }
     }
+}
+
+val dummyQuotes = List(10) { i ->
+    QuoteUiModel(
+        id = i.toString(),
+        content = "이 책을 읽으면서 꿈에 대한 새로운 관점을 얻게 되었다. $i",
+        page = 10 + i,
+        date = "2024.01.10"
+    )
 }
