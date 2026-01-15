7bdd36f8217243c668a71e1f5f8b27a898a806b5
JangMuho <muhojang.dev@gmail.com>
2026-01-15 23:14:09 +0900

    style: And03AppBar 패딩 설정을 safeDrawing 인셋으로 변경 - 기존 statusBarsPadding을 safeDrawing 인셋으로 교체하여 가로 모드 및 상단 레이아웃 대응 개선 - 더 이상 앱 바의 아이콘이 시스템 바 영역에 겹치지 않음

--- app/src/main/java/com/boostcamp/and03/ui/component/And03AppBar.kt
+++ app/src/main/java/com/boostcamp/and03/ui/component/And03AppBar.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -33,7 +38,11 @@ fun And03AppBar(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .statusBarsPadding()
+            .windowInsetsPadding(
+                WindowInsets.safeDrawing.only(
+                    WindowInsetsSides.Horizontal + WindowInsetsSides.Top
+                )
+            )
             .height(APP_BAR_HEIGHT)
             .padding(horizontal = And03Padding.PADDING_M),
         contentAlignment = Alignment.Center
