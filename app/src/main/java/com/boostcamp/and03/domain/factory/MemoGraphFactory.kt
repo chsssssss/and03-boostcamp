fbade81a5a62a0e738500133ea159500eb849227
chsssssss <chaheesun42@gmail.com>
2026-01-20 05:23:02 +0900

    refactor: MemoGraphFactory 변환 로직에 데이터 매퍼 적용
    - response에서 domain 모델로의 변환 로직을 개별 매퍼 함수(toDomain)로 분리하여 코드 간소화 및 재사용성 향상

--- app/src/main/java/com/boostcamp/and03/domain/factory/MemoGraphFactory.kt
+++ app/src/main/java/com/boostcamp/and03/domain/factory/MemoGraphFactory.kt
@@ -1,10 +1,8 @@
 package com.boostcamp.and03.domain.factory
 
+import com.boostcamp.and03.data.mapper.toDomain
 import com.boostcamp.and03.data.model.response.memo.CanvasMemoResponse
-import com.boostcamp.and03.domain.model.Edge
 import com.boostcamp.and03.domain.model.MemoGraph
-import com.boostcamp.and03.domain.model.MemoNode
-import androidx.compose.ui.geometry.Offset
 
 object MemoGraphFactory {
 
@@ -17,26 +15,9 @@ object MemoGraphFactory {
 
     // CanvasMemoResponse → MemoGraph 변환
     fun fromResponse(response: CanvasMemoResponse): MemoGraph {
-        val nodesMap = response.graph.nodes.associate { node ->
-            node.id to MemoNode(
-                id = node.id,
-                title = node.title,
-                content = node.content,
-                offset = Offset(node.x, node.y)
-            )
-        }
-
-        val edgesList = response.graph.edges.map { edge ->
-            Edge(
-                fromId = edge.fromNodeId,
-                toId = edge.toNodeId,
-                name = edge.relationText
-            )
-        }
-
         return MemoGraph(
-            nodes = nodesMap,
-            edges = edgesList
+            nodes = response.graph.nodes.associate { it.id to it.toDomain() },
+            edges = response.graph.edges.map { it.toDomain() }
         )
     }
 }
