2ab079b53be8fa4d8663f013fb39edadc17c6836
chsssssss <chaheesun42@gmail.com>
2026-01-20 10:26:10 +0900

    refactor: QuoteCard의 수정 및 삭제 버튼 노출 여부 제어
    - onClickDelete 및 onClickEdit을 선택적 매개변수로 변경하고, 값이 있을 때만 드롭다운 메뉴 표시하도록 개선

--- app/src/main/java/com/boostcamp/and03/ui/screen/bookdetail/component/QuoteCard.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/bookdetail/component/QuoteCard.kt
@@ -31,9 +31,9 @@ import com.boostcamp.and03.ui.theme.And03Theme
 fun QuoteCard(
     quote: QuoteUiModel,
     onClick: () -> Unit,
-    onClickDelete: () -> Unit,
-    onClickEdit: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onClickDelete: (() -> Unit)? = null,
+    onClickEdit: (() -> Unit)? = null,
 ) {
     Card(
         modifier = modifier
@@ -60,33 +60,36 @@ fun QuoteCard(
                         top = And03Padding.PADDING_XL,
                     )
                 )
-                DropdownMenuContainer(
-                    trigger = { onClick ->
-                        IconButton(onClick = onClick) {
-                            Icon(
-                                imageVector = ImageVector.vectorResource(R.drawable.ic_more_vert_filled),
-                                contentDescription = stringResource(R.string.cd_more_options)
+                if (onClickDelete != null && onClickEdit != null) {
+                    DropdownMenuContainer(
+                        trigger = { onClick ->
+                            IconButton(onClick = onClick) {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(R.drawable.ic_more_vert_filled),
+                                    contentDescription = stringResource(R.string.cd_more_options)
+                                )
+                            }
+                        },
+                        menuContent = { closeMenu ->
+                            DropdownMenuItem(
+                                text = { Text(stringResource(R.string.more_vert_edit)) },
+                                onClick = {
+                                    closeMenu()
+                                    onClickEdit()
+                                }
+                            )
+
+                            DropdownMenuItem(
+                                text = { Text(stringResource(R.string.more_vert_delete)) },
+                                onClick = {
+                                    closeMenu()
+                                    onClickDelete()
+                                }
                             )
                         }
-                    },
-                    menuContent = { closeMenu ->
-                        DropdownMenuItem(
-                            text = { Text(stringResource(R.string.more_vert_edit)) },
-                            onClick = {
-                                closeMenu()
-                                onClickEdit()
-                            }
-                        )
+                    )
+                }
 
-                        DropdownMenuItem(
-                            text = { Text(stringResource(R.string.more_vert_delete)) },
-                            onClick = {
-                                closeMenu()
-                                onClickDelete()
-                            }
-                        )
-                    }
-                )
             }
             Row(
                 modifier = Modifier
