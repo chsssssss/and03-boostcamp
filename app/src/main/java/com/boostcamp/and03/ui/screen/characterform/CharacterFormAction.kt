ce165698f82eee31d139f1279f5bcd65169ff239
JangMuho <muhojang.dev@gmail.com>
2026-01-22 20:33:03 +0900

    feat: CharacterForm UI 인터액션 처리를 위한 Action 및 Event 패턴 구현 - CharacterFormAction 및 CharacterFormEvent 정의 - ViewModel 내 개별 상태 변경 메서드를 onAction으로 통합 - navigation 처리를 위한 Single Live Event(Channel) 추가

---
+++ app/src/main/java/com/boostcamp/and03/ui/screen/characterform/CharacterFormAction.kt
@@ -0,0 +1,14 @@
+package com.boostcamp.and03.ui.screen.characterform
+
+sealed interface CharacterFormAction {
+
+    data object OnBackClick: CharacterFormAction
+
+    data object OnSaveClick: CharacterFormAction
+
+    data class OnNameChange(val name: String): CharacterFormAction
+
+    data class OnRoleChange(val role: String): CharacterFormAction
+
+    data class OnDescriptionChange(val description: String): CharacterFormAction
+}
--- app/src/main/java/com/boostcamp/and03/ui/screen/characterform/CharacterFormViewModel.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/characterform/CharacterFormViewModel.kt
@@ -1,6 +1,5 @@
 package com.boostcamp.and03.ui.screen.characterform
 
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,8 +8,10 @@ import com.boostcamp.and03.data.repository.book_storage.BookStorageRepository
 import com.boostcamp.and03.ui.navigation.Route
 import com.boostcamp.and03.ui.screen.bookdetail.model.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -24,10 +25,33 @@ class CharacterFormViewModel @Inject constructor (
     private val bookId = characterFormRoute.bookId
     private val characterId = characterFormRoute.characterId
 
+    private val userId: String = "O12OmGoVY8FPYFElNjKN"
+
     private val _uiState = MutableStateFlow(CharacterFormUiState())
     val uiState = _uiState.asStateFlow()
 
-    private val userId: String = "O12OmGoVY8FPYFElNjKN"
+    private val _event = Channel<CharacterFormEvent>()
+    val event = _event.receiveAsFlow()
+
+    fun onAction(action: CharacterFormAction) {
+        when (action) {
+            CharacterFormAction.OnBackClick -> _event.trySend(CharacterFormEvent.NavigateBack)
+
+            CharacterFormAction.OnSaveClick -> {
+                viewModelScope.launch {
+                    saveCharacter()
+                }
+
+                _event.trySend(CharacterFormEvent.NavigateBack)
+            }
+
+            is CharacterFormAction.OnNameChange -> _uiState.update { it.copy(name = action.name) }
+
+            is CharacterFormAction.OnRoleChange -> _uiState.update { it.copy(role = action.role) }
+
+            is CharacterFormAction.OnDescriptionChange -> _uiState.update { it.copy(description = action.description) }
+        }
+    }
 
     init {
         viewModelScope.launch {
@@ -52,27 +76,7 @@ class CharacterFormViewModel @Inject constructor (
         }
     }
 
-    fun changeName(name: String) {
-        _uiState.update { it.copy(name = name) }
-    }
-
-    fun changeRole(role: String) {
-        _uiState.update { it.copy(role = role) }
-    }
-
-    fun changeDescription(description: String) {
-        _uiState.update { it.copy(description = description) }
-    }
-
-    fun changeIconColor(color: Color) {
-        _uiState.update { it.copy(iconColor = color) }
-    }
-
-    fun changeImageUrl(url: String) {
-        _uiState.update { it.copy(imageUrl = url) }
-    }
-
-    suspend fun saveCharacter() {
+    private suspend fun saveCharacter() {
         if (characterId.isBlank()) {
             bookStorageRepository.addCharacter(
                 userId = userId,
