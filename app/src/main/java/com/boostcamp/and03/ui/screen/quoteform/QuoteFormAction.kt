f254adf4422c49bfec8da354b2f01f7ffc67e2ba
JangMuho <muhojang.dev@gmail.com>
2026-01-23 00:23:04 +0900

    feat: 인상 깊은 구절 화면 ViewModel 구현

--- app/src/main/java/com/boostcamp/and03/ui/screen/quoteform/QuoteFormAction.kt
+++ app/src/main/java/com/boostcamp/and03/ui/screen/quoteform/QuoteFormAction.kt
@@ -6,7 +6,7 @@ sealed interface QuoteFormAction {
 
     data object OnSaveClick: QuoteFormAction
 
-    data class OnContentChange(val content: String): QuoteFormAction
+    data class OnQuoteChange(val quote: String): QuoteFormAction
 
     data class OnPageChange(val page: String): QuoteFormAction
 }
