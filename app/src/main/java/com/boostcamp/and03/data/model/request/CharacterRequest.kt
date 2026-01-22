134cbecea59064218746c2614d78ee636e39e254
JangMuho <muhojang.dev@gmail.com>
2026-01-22 16:40:54 +0900

    feat: CharacterUiModel을 CharacterRequest로 변환하는 확장 함수 추가 - UI 모델을 데이터 레이어의 요청 모델로 변환하여 파이어베이스 요청 호출 시 사용 가능하도록 개선

--- app/src/main/java/com/boostcamp/and03/data/model/request/CharacterRequest.kt
+++ app/src/main/java/com/boostcamp/and03/data/model/request/CharacterRequest.kt
@@ -1,5 +1,6 @@
 package com.boostcamp.and03.data.model.request
 
+import com.boostcamp.and03.ui.screen.bookdetail.model.CharacterUiModel
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,4 +8,10 @@ data class CharacterRequest(
     val role: String = "",
     val description: String = "",
     val name: String = ""
+)
+
+fun CharacterUiModel.toRequest() = CharacterRequest(
+    role = role,
+    description = description,
+    name = name
 )
