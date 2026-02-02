6e361d3637cb79e35b960e36a5ef4eb16ecdfbef
JangMuho <muhojang.dev@gmail.com>
2026-02-02 16:42:46 +0900

    build: 앱 버전 업데이트 (1.0.7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.boostcamp.and03"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
