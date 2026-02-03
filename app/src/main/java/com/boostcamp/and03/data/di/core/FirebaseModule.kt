2e86cf65516dd4dd33d875520abdec059ce5f402
chsssssss <chaheesun42@gmail.com>
2026-02-04 06:59:52 +0900

    refactor: FirebaseStorage 인스턴스 접근 방식 변경
    - Firebase KTX를 사용하여 인스턴스 획득 방식 일관성 유지

--- app/src/main/java/com/boostcamp/and03/data/di/core/FirebaseModule.kt
+++ app/src/main/java/com/boostcamp/and03/data/di/core/FirebaseModule.kt
@@ -4,6 +4,7 @@ import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,6 +24,6 @@ object FirebaseModule {
     @Provides
     @Singleton
     fun provideFirebaseStorage(): FirebaseStorage =
-        FirebaseStorage.getInstance()
+        Firebase.storage
 
 }
