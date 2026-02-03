6109d7a5ea88c3d60b31b1bdc215b06f145dc623
chsssssss <chaheesun42@gmail.com>
2026-02-04 05:10:08 +0900

    refactor: 이미지 URI 생성 방식을 캐시 파일 기반으로 변경
    - MediaStore를 통한 이미지 저장 방식을 FileProvider와 캐시 파일을 사용하는 방식으로 개선하여 효율성 증대 및 저장소 권한 의존성 감소

--- app/src/main/java/com/boostcamp/and03/ui/util/ImageExtension.kt
+++ app/src/main/java/com/boostcamp/and03/ui/util/ImageExtension.kt
@@ -1,14 +1,27 @@
 package com.boostcamp.and03.ui.util
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.net.Uri
-import android.provider.MediaStore.Images
-import java.io.ByteArrayOutputStream
+import androidx.core.content.FileProvider
+import java.io.File
 
-fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
-    val bytes = ByteArrayOutputStream()
-    inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-    val path = Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
-    return Uri.parse(path)
+//fun getImageUri(context: Context, inImage: Bitmap): Uri {
+//    return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//        createTempImageUri(context)
+//    }
+//    else {
+//        saveTheImageLegacyStyle(context, inImage)
+//    }
+//}
+
+fun createTempImageUri(context: Context): Uri {
+    val file = File.createTempFile(
+        "temp_${System.currentTimeMillis()}", ".jpg",
+        context.cacheDir
+    )
+    return FileProvider.getUriForFile(
+        context,
+        "${context.packageName}.fileprovider",
+        file
+    )
 }
