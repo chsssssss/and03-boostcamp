bcc49a1a33b42efb3377c9356cf49cb6d7912e26
JangMuho <muhojang.dev@gmail.com>
2026-02-05 07:04:03 +0900

    feat: 네트워크 연결을 감지하는 NetworkManager 클래스 구현

---
+++ app/src/main/java/com/boostcamp/and03/network/NetworkManager.kt
@@ -0,0 +1,31 @@
+package com.boostcamp.and03.network
+
+import android.net.ConnectivityManager
+import android.net.Network
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+class NetworkManager @Inject constructor(
+    private val connectivityManager: ConnectivityManager
+) {
+    private val _isConnected = MutableStateFlow(false)
+    val isConnected = _isConnected
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            _isConnected.value = true
+        }
+
+        override fun onLost(network: Network) {
+            _isConnected.value = false
+        }
+    }
+
+    fun registerNetworkCallback() {
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
+    }
+
+    fun unRegisterNetworkCallback() {
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+}
